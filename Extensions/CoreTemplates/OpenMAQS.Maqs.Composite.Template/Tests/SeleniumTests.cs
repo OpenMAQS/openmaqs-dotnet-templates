483cd250fbdf345c2760c59d1b9df46bd1130058
whr0530 <bill.roske@daugherty.com>
2024-06-06 14:23:21 -0500

    Removing erroneous code in composite

--- Extensions/CoreTemplates/OpenMAQS.Maqs.Composite.Template/Tests/SeleniumTests.cs
+++ Extensions/CoreTemplates/OpenMAQS.Maqs.Composite.Template/Tests/SeleniumTests.cs
@@ -16,32 +16,6 @@ namespace Tests
     public class SeleniumTests : BaseSeleniumTest
     {
         /// <summary>
-        /// Do database setup for test run
-        /// </summary>
-        // [ClassInitialize] - Disabled because this step will fail as the template does not include access to a test database
-        public static void TestSetup(TestContext context)
-        {
-            // Do database setup
-            using (DatabaseDriver wrapper = new DatabaseDriver(DatabaseConfig.GetProviderTypeString(), DatabaseConfig.GetConnectionString()))
-            {
-                var result = wrapper.Query("getStateAbbrevMatch", new { StateAbbreviation = "MN" }, commandType: CommandType.StoredProcedure);
-                Assert.AreEqual(1, result.Count(), "Expected 1 state abbreviation to be returned.");
-            }
-        }
-
-        /// <summary>
-        /// Do post test run web service cleanup
-        /// </summary>
-        // [ClassCleanup] - Disabled because this step will fail against the current base service
-        public static void TestCleanup()
-        {
-            // Do web service post run cleanup
-            WebServiceDriver client = new WebServiceDriver(new Uri(WebServiceConfig.GetWebServiceUri()));
-            string result = client.Delete("/api/String/Delete/1", "text/plain", true);
-            Assert.AreEqual(string.Empty, result);
-        }
-
-        /// <summary>
         /// Open page test
         /// </summary>
         [TestMethod]
